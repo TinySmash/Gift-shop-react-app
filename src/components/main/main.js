45e562fda49ea65f787a2328ed5cd798bec3d3b7
Islam Elgueniari <islam.gueniari@gmail.com>
2022-11-09 21:59:55 +0100

    toggle the burger menu button

--- src/components/main/main.js
+++ src/components/main/main.js
@@ -6,7 +6,22 @@ import './main.css';
 
 function Main(){
 
+    let menuOpened = false;
 
+    const menuToggle = (element) => {
+        element.preventDefault();
+        let menuButton = document.querySelector('.menu-btn');
+        if(!menuOpened){
+            menuButton.classList.add('open');
+        }else if(menuOpened){
+            menuButton.classList.remove('open');
+        }
+        menuOpened = !menuOpened ;
+    }
+
+    const search = (e) => {
+        e.preventDefault();
+    }
 
 
     return ( 
@@ -14,8 +29,8 @@ function Main(){
             <h1 id='Title'>Your favorite<br></br>Gifts shop</h1>
         
             <nav id='navbar'>
-                <div id='menu-btn'>
-                    <div id='menu-btn-burger'></div>
+                <div className='menu-btn' onClick={menuToggle}>
+                    <div className='menu-btn-burger'></div>
                 </div>
                 <ul>
                     <li className='menu' id='menu1'><Link className='anchor-menu'>Profile</Link></li>
@@ -24,7 +39,7 @@ function Main(){
                 </ul>
                 <form id='mainForm'>
                     <input id='mainInput'/>
-                    <button id='search'>search</button>
+                    <button id='search' onClick={search}>search</button>
                 </form>
             </nav>
         </div>
